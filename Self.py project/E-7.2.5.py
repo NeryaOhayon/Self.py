5b843b7b052632b2a7594888db4d4b126db4f936
NeryaOhayon <nerya242@gmail.com>
2022-01-28 00:08:10 +0200

    The final change - that works

--- Self.py project/E-7.2.5.py
+++ Self.py project/E-7.2.5.py
@@ -1,105 +1,35 @@
 print()
 
-#גם בקוד של אגם יש טעות, אין התייחסות לזה שהאותיות ברצף 
-#לתקן שיהיה קשור לרצף של האותיות
-#def sequence_del(my_str):
- #   lst = list(my_str)
-  #  str = ""
 
-   # for chr in lst:
-        
-    #    while lst.count(chr) > 1:
-     #       lst.remove(chr)
+def sequence_del(my_str):
+    """The function takes a string and deletes the letters that appear in sequence. That is, the function returns a string in which only one character appears from each sequence of identical characters in the input string.
+    :param my_str: Receives from the user a string to decrypt.
+    :variable bar: The final string is stored in it.
+    :type my_str: "str". 
+    :type bar: "str". 
+    :return: The function returns a string without repeating the same signal several times in succession.
+    :rtype: "str". 
+    """
 
-    #for i in lst:
-     #   str += i
+    bar = ''
+    for chr in my_str:
+        if bar == '' or chr != bar[len(bar)-1]:
+            bar += chr
+    return bar
+
+
+
+def main():
+    print()
+    print(sequence_del("Heeyyy   yyouuuu!!!"))
+    print()
+    print(sequence_del("ppyyyyythhhhhooonnnnn"))
+    print()
+    print(sequence_del("SSSSsssshhhh"))
+    print()
+
+if __name__ == "__main__":
+    main()
     
-    #return str 
+help(sequence_del)
 
-
-#def main():
- #   print()
-  #  print(sequence_del("pppyyyyyythhhhhooonnnnn"))
-   # print()
-    #print(sequence_del("Heeyyy   yyouuuu!!!"))
-#if __name__ == "__main__":
- #   main()
-    
-
-
-#next_chr = lst.index(chr) + 1
-#chr == lst[next_chr]:
-
-#lst.count(chr) > 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# לבדוק מה צריך להוסיף
-
-#def sequence_del(my_str):
- #   my_str_list = list(my_str)
-   
-  #  for i in my_str_list:
-   #     if my_str_list.count(i) > 1:
-    #       my_str_list.remove(i)
-
-     #   else:
-      #      continue
-           
-
-    #return my_str_list
-
-
-#print(sequence_del("pppyyyyyythhhhhooonnnnn"))
-#print()
-
-
-
-
-
-#def sequence_del(my_str):
- #   my_str_list = list(my_str)
-  #  new_list = []
-   # for i in my_str_list:
-    #    if my_str_list.count(i) == 1:
-     #       new_list += [i]
-            
-
-      #  elif (my_str_list.count(i) > 1) and (i in new_list == False):
-       #     new_list += [i]
-            
-
-    #return new_list
-
-
-
-
-# לבדוק איך לעקוף את זה שזה מחוץ לטווח
-
-def sequence(my_str):
-    my_str_list = list(my_str)
-    
-    
-    for i in my_str_list:
-        next_i = my_str_list.index(i) + 1
-
-        while i == my_str_list[next_i]:
-            my_str_list.remove(i)
-            #my_str.translate({ord(i): None})
-
-    return my_str_list
-
-print(sequence("pppyyyyyythhhhhooonnnnn"))
-print()
